b4bd45d40e199ef9ef2715a7a622dcbdd74b05b2
Vladislav Dragushin <maxhc@inbox.ru>
2023-02-03 01:55:22 +0300

    View price into manager UI

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
+from django.db.models import F, Sum
 
 
 class Restaurant(models.Model):
@@ -124,6 +125,16 @@ class RestaurantMenuItem(models.Model):
         return f"{self.restaurant.name} - {self.product.name}"
 
 
+class OrderQuerySet(models.QuerySet):
+    def with_price(self):
+        price = self.prefetch_related('orders') \
+            .annotate(
+                price=Sum(F('orders__product__price') * F('orders__quantity'))
+            )
+
+        return price
+
+
 class Order(models.Model):
     firstname = models.CharField(
         max_length=20,
@@ -140,6 +151,8 @@ class Order(models.Model):
         max_length=100,
         verbose_name='Адрес'
     )
+
+    objects = OrderQuerySet.as_manager()
 
     class Meta:
         verbose_name = 'Заказ'
