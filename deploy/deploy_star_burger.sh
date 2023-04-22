8f1222b310d466def154760dc1d198e9a4e06ceb
Vladuslav Dragushin <maxhc@inbox.ru>
2023-04-22 16:30:28 +0300

    Update deploy.sh

--- deploy/deploy_star_burger.sh
+++ deploy/deploy_star_burger.sh
@@ -6,9 +6,9 @@ echo "git ok"
 npm ci
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 echo "frontend ok"
-../env/bin/python3.10 -m pip install -r requirements.txt
-../env/bin/python3.10 manage.py collectstatic --noinput
-../env/bin/python3.10 manage.py migrate --noinput
+./env/bin/python3.10 -m pip install -r requirements.txt
+./env/bin/python3.10 manage.py collectstatic --noinput
+./env/bin/python3.10 manage.py migrate --noinput
 echo "backend ok"
 systemctl daemon-reload
 systemctl reload nginx
@@ -17,5 +17,6 @@ systemctl start django.service
 echo "systemctl start ok"
 last_commit_hash=$(git rev-parse HEAD)
 echo $last_commit_hash
-curl -H "X-Rollbar-Access-Token: ROLLBAR_ACCESS_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production", "revision": "'${last_commit_hash}'", "rollbar_name": "kiablunovskii", "local_username": "root", "comment": "auto deploy", "status": "succeeded"}'
+source ./star_burger/.env
+curl -H "X-Rollbar-Access-Token: ${ROLLBAR_ACCESS_TOKEN}" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production", "revision": "'${last_commit_hash}'", "rollbar_name": "kiablunovskii", "local_username": "root", "comment": "auto deploy", "status": "succeeded"}'
 echo "End deploy!"
