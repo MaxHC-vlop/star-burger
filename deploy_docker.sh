3b6ec7a8f29e6e8e02d893709e83c38ff80f9c82
Vladuslav Dragushin <maxhc@inbox.ru>
2023-05-31 19:30:37 +0300

    Added deploy script

---
+++ deploy_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git pull origin master
+echo "git ok"
+docker compose -f docker-compose.prod_ssl.yml build
+docker compose -f docker-compose.prod_ssl.yml up -d
+echo "Docker-compose ok"
+
+last_commit_hash=$(git rev-parse HEAD)
+echo $last_commit_hash
+
+source .env
+curl -H "X-Rollbar-Access-Token: ${ROLLBAR_ACCESS_TOKEN}" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production", "revision": "'${last_commit_hash}'", "rollbar_name": "kiablunovskii", "local_username": "root", "comment": "auto deploy", "status": "succeeded"}'
+echo "End docker deploy!"
